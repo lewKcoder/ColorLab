f86748fc2a8e4d3b9100aa135e5d5ef892c619d1
lewKcoder <7kamimura74@gmail.com>
2024-09-19 02:18:42 +0900

    fix: :bug: Removed dupulicate title

--- app/components/loading/loading.tsx
+++ app/components/loading/loading.tsx
@@ -1,21 +1,16 @@
 import { FunctionComponent } from "react";
 import styles from "./styles.module.scss";
-import { Heading } from "../heading";
 
 export const Loading: FunctionComponent = () => {
   return (
-    <>
-      <Heading />
+    <div className={styles.container}>
+      <div className={styles.content} />
 
-      <div className={styles.container}>
-        <div className={styles.content} />
-
-        <div className={styles.indicator}>
-          <div className={styles.point1} />
-          <div className={styles.point2} />
-          <div className={styles.point3} />
-        </div>
+      <div className={styles.indicator}>
+        <div className={styles.point1} />
+        <div className={styles.point2} />
+        <div className={styles.point3} />
       </div>
-    </>
+    </div>
   );
 };
