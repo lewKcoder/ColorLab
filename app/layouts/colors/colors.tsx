e7053102068bf514eb36be6d64a148b01004506c
lewKcoder <7kamimura74@gmail.com>
2024-09-19 02:16:28 +0900

    refactor: :recycle: Refactaring structure

--- app/layouts/colors/colors.tsx
+++ app/layouts/colors/colors.tsx
@@ -1,18 +1,12 @@
 import { Button, ColorElement, Loading } from "@/app/components";
 import styles from "./styles.module.scss";
-import { Component } from "./types";
-import { useEffect } from "react";
-import { blendMultipleColors } from "@/app/utils";
+import { FunctionComponent, useEffect, useState } from "react";
+import { blendMultipleColors, getTextColor } from "@/app/utils";
 
-export const LayoutColors: Component = (props) => {
-  const {
-    colors,
-    isThreeColors,
-    resultColor,
-    setColors,
-    setIsThreeColors,
-    setResultColor,
-  } = props;
+export const LayoutColors: FunctionComponent = () => {
+  const [colors, setColors] = useState(["#f5b4b4", "#f85c5c"]);
+  const [isThreeColors, setIsThreeColors] = useState(false);
+  const [resultColor, setResultColor] = useState("");
 
   useEffect(() => {
     setResultColor(blendMultipleColors(colors));
@@ -23,25 +17,38 @@ export const LayoutColors: Component = (props) => {
   }
 
   return (
-    <div className={styles.color_scheme}>
-      {colors.map((color, index) => (
-        <ColorElement
-          key={color}
-          id={`color-pick${index}`}
-          onChange={(e) => {
-            colors[index] = e.target.value;
-            setColors([...colors]);
-          }}
-          color={color}
-        />
-      ))}
+    <div className={styles.content}>
+      <h2 className={`${styles.result_color} result_color`}>{resultColor}</h2>
 
-      <div className={styles.button}>
-        <Button
-          isThreeColors={isThreeColors}
-          onClick={() => setIsThreeColors(!isThreeColors)}
-        />
+      <div className={styles.color_scheme}>
+        {colors.map((color, index) => (
+          <ColorElement
+            key={index}
+            id={`color-pick${index}`}
+            onChange={(e) => {
+              colors[index] = e.target.value;
+              setColors([...colors]);
+            }}
+            color={color}
+          />
+        ))}
+
+        <div className={styles.button}>
+          <Button
+            isThreeColors={isThreeColors}
+            onClick={() => setIsThreeColors(!isThreeColors)}
+          />
+        </div>
       </div>
+
+      <style jsx>{`
+        .container {
+          background-color: ${resultColor};
+        }
+        .result_color {
+          color: ${getTextColor(resultColor)};
+        }
+      `}</style>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,15 @@
 "use client";
 
-import { useState } from "react";
 import { Heading } from "./components";
 import styles from "./styles.module.scss";
-import { getTextColor } from "./utils";
 import { LayoutColors } from "./layouts";
 
 export default function Home() {
-  const [colors, setColors] = useState(["#f5b4b4", "#f85c5c"]);
-  const [resultColor, setResultColor] = useState("");
-  const [isThreeColors, setIsThreeColors] = useState(false);
-
   return (
-    <div className={`${styles.container} container`}>
-      <main className={styles.main}>
-        <Heading />
+    <main className={`${styles.container} container`}>
+      <Heading />
 
-        <div className={styles.content}>
-          <h2 className={`${styles.result_color} result_color`}>
-            {resultColor}
-          </h2>
-
-          <LayoutColors
-            colors={colors}
-            isThreeColors={isThreeColors}
-            resultColor={resultColor}
-            setColors={setColors}
-            setIsThreeColors={setIsThreeColors}
-            setResultColor={setResultColor}
-          />
-        </div>
-      </main>
-
-      <style jsx>{`
-        .container {
-          background-color: ${resultColor};
-        }
-        .result_color {
-          color: ${getTextColor(resultColor)};
-        }
-      `}</style>
-    </div>
+      <LayoutColors />
+    </main>
   );
 }
