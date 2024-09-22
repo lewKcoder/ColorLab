6387524d1ba4be6c193805bcf0cfe307082f38d0
lewKcoder <7kamimura74@gmail.com>
2024-09-22 17:58:53 +0900

    feat: :sparkles: Added ogp and favicon images

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.scss";
 
 export const metadata: Metadata = {
-  title: "Color lab",
+  title: "chromix",
   description:
     "この色とあの色を混ぜた時の色は？その色のカラーパレットは？試してみよう！",
 };
@@ -15,6 +15,23 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <link rel="icon" type="image/png" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <meta property="og:title" content="chromix" />
+        <meta
+          property="og:description"
+          content="この色とあの色を混ぜた時の色は？その色のカラーパレットは？試してみよう！"
+        />
+        <meta property="og:image" content="/ogp.png" />
+        <meta property="og:url" content="https://chromix.xyz/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="chromix" />
+        <meta
+          name="twitter:description"
+          content="この色とあの色を混ぜた時の色は？その色のカラーパレットは？試してみよう！"
+        />
+        <meta name="twitter:image" content="/ogp.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
