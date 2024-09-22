67436e47db8c87e5a021684bb107ab15c36b7d66
lewKcoder <7kamimura74@gmail.com>
2024-09-22 14:32:19 +0900

    refactor: :recycle: Refacterd to unuse svg circle

--- app/components/color-element/color-element.tsx
+++ app/components/color-element/color-element.tsx
@@ -5,24 +5,17 @@ export const ColorElement: Component = (props) => {
   const { id, onChange, color } = props;
 
   return (
-    <>
+    <label
+      htmlFor={id}
+      className={styles.label}
+      style={{ backgroundColor: color }}
+    >
       <input
         type="color"
         id={id}
         className={styles.input}
         onChange={onChange}
       />
-      <label htmlFor={id} className={styles.label}>
-        <svg
-          width="200"
-          height="200"
-          viewBox="0 0 200 200"
-          fill={color}
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="100" cy="100" r="99.5" />
-        </svg>
-      </label>
-    </>
+    </label>
   );
 };
